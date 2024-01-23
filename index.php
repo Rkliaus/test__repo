c940b5b4ce070ba40e96608e9a3a1db066c4c580
Rkliaus <kliausroman@gmail.com>
2024-01-23 15:53:08 +0300

    new file:   index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+// 1. 
+$x = 5;
+$x = 5+3;
+echo $x;
+echo "<br><hr>";
+// 2. 
+$a = 10;
+$b = 7;
+$a= $a / $b;
+echo $a;
+echo "<br><hr>";
+// 3. 
+$name = "Алиса";
+$age = 25;
+echo "Меня зовут $name и мне $age лет";
+echo "<br><hr>";
+// 4.
+$y =12;
+$y = $y* 2;
+echo $y;
+echo "<br><hr>";
+// 5.
+$x=6;
+$y=4;
+$x=$x*$y;
+echo $x;
+echo "<br><hr>";
+// 6.
+$var1 = 7;
+$var2 = 3;
+$var3 = $var1 + $var2;
+echo $var3;
+echo "<br><hr>";
+// 7.
+$stringName = "меня зовут пластилин,";
+$stringName = $stringName."мне нравится Виктор Цой";
+echo $stringName;
+echo "<br><hr>";
+// 8.
+$z=5;
+++$z;
+echo $z;
+echo "<br><hr>";
+// 9.
+$stringName = "меня зовут Виктор Цой,";
+$stringIlike = "мне нравится пластилин";
+$stringSummary = $stringName.$stringIlike;
+echo $stringSummary;
+echo "<br><hr>";
+// 10.
+$digit = "5";
+$number = 13;
+echo $digit+$number;
